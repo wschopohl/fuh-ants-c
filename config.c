38135eff06856da7dd98e589aa62e7f84d9e02c2
Wieland Schopohl <info@medien-produkt.de>
2023-09-10 20:42:01 +0200

    added new world and changed some configs

--- config.c
+++ config.c
@@ -2,7 +2,7 @@
 
 Configuration Config = {
     .threads = 4,
-    .max_ants = 500,
+    .max_ants = 5000,
     .spawn_rate = 10.0,
     .ant_angle_step = 5,
     .ant_angle_variation = 30.0,
@@ -11,11 +11,11 @@ Configuration Config = {
     .ant_wall_search_angle = 15.0,
     .ant_nest_radius = 20,
     .ant_nest_sense_radius = 100,
-    .pheromone_map_scale = 0.5,
+    .pheromone_map_scale = 1.0,
     .pheromone_drop_strength = 40,
     .pheromone_decline_step = 200,
     .pheromone_decay_strength = 2,
-    .pheromone_decay_rate = 1,
-    .ant_sense_distance = 15,
+    .pheromone_decay_rate = 2,
+    .ant_sense_distance = 30,
     .ant_field_of_view = 180.0
 };
--- main.c
+++ main.c
@@ -11,6 +11,7 @@ int main(void)
     // WorldInit(1150, 680, "assets/maps/map_maze_2.png", "assets/maps/map_maze_food_3.png", (Vector2){.x=430,.y=390});
     WorldInit(1150, 680, "assets/maps/map_maze_thick.png", "assets/maps/map_maze_food_3.png", (Vector2){.x=430,.y=390});
     // WorldInit(1150, 680, "assets/maps/map_dyno.png", "assets/maps/map_dyno_food.png", (Vector2){.x=187,.y=121});
+    // WorldInit(1150, 680, "assets/maps/map_thank_you_bg.png", "assets/maps/map_thank_you_food.png", (Vector2){.x=575,.y=60});
 
     SimulationStart();
 
